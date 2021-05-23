223f22cb6259fd156380a29627331bbc1ecdc26c
Adam <adamchehab@gmail.com>
2021-05-23 19:50:36 +0300

    First commit

--- main.asm
+++ main.asm
@@ -13,6 +13,7 @@ section '.data' data readable writable
 
 	; strings to invite user to input
 	strA db 'Enter A: ', 0
+	strB db 'Enter B: ', 0
 	strC db 'Enter C: ', 0
 	strD db 'Enter D: ', 0
 
@@ -24,12 +25,12 @@ section '.data' data readable writable
 	
 	; input numbers
 	a dd ?
+	b dd ? 
 	c dd ?
 	d dd ?
 
 	; variable to store if a is odd or not: if = 1 then odd if = 0 then notOdd
-	b dd ? 
-
+	odd dd ?
 
 ; section for code
 section '.code' code readable executable
@@ -44,6 +45,15 @@ section '.code' code readable executable
 
         ; read input to A
         push a
+        push spaceStr
+        call [scanf]
+
+		; invite to enter B
+        push strB
+        call [printf]
+
+        ; read input to B
+        push b
         push spaceStr
         call [scanf]
 
@@ -71,53 +81,77 @@ section '.code' code readable executable
         cdq
         mov ecx, 2
         idiv ecx
-        mov [b], edx
+        mov [odd], edx
 
 
 	; SOLVE EQUATION
 
-        ; (2c + d – 52) / (a/4 + 1)
+        ; (2c – d/3) / (b – a/4).
 
-        ; (2c + d – 52)
-        mov eax, [c]
-        mov ebx, 2
-        imul ebx
-        add eax, [d]
-        sub eax, 52
-        mov ebx, eax
-        ; (result is in EBX)
+		; (b – a/4)
 
-        ;ecx=(a/4-1)
-        mov eax, [a]
-        cdq
-        mov ecx, 4
-        idiv ecx
+			; (a/4)
+			mov eax, [a]
+			cdq
+			mov ecx, 4
+			idiv ecx
 
-        ; MOD OR DIV - we enter this code if a is not odd
-        cmp [b], 0
-        jne isOdd
-        	mov eax, edx
-		
-        isOdd:
-        add eax, 1
-        mov ecx, eax
-        ; (result is in ECX)
+			; MOD OR DIV
+			cmp [odd], 0
+			jne isOdd
+				mov eax, edx
+			isOdd:
+			mov ebx, eax
 
-        ;eax=(2*c-d+23)/(a/4-1)
-        mov eax, ebx
-        cdq
-        idiv ecx
+			; (b - a/4)
+			mov eax, [b]
+			sub eax, ebx
+			mov esi, eax
+			; (result is now in ESI )
 
-        ; MOD OR DIV - we enter this code if a is not odd
-        cmp [b], 0
-        jne isOdd1
-        	mov eax, edx
-		
-        isOdd1:
-        ; (result is in EAX)
+		; (2c – d/3)
+
+			; (2*c)
+			mov eax, [c]
+     		mov ebx, 2
+       		imul ebx
+			mov edi, eax 
+			; (result is now in EDI)
+
+			; (d/3)
+			mov eax, [d]
+			cdq
+			mov ecx, 3
+			idiv ecx
+
+			; MOD OR DIV
+			cmp [odd], 0
+			jne isOdd1
+				mov eax, edx
+			isOdd1:
+			mov ebx, eax
+			; (is now in EBX)
+
+			; (2c - d/3)
+			mov eax, edi		; move 2*c to eax
+			sub eax, ebx
+			; (result is now in EAX )
+
+		; (2c – d/3) / (b – a/4)
+			cdq
+			mov ecx, esi		; move (b – a/4) to ecx
+			idiv ecx
+
+			; MOD OR DIV
+			cmp [odd], 0
+			jne isOdd2
+				mov eax, edx
+			isOdd2:
+			mov ebx, eax
+			; (is now in EBX)
 
 	; OUTPUT RESULT
-        push eax
+        push ebx
         push resStr
         call [printf]
 
